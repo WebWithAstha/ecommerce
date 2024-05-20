70a0846b4620f3fb9bb2f769bb811d71606aaca2
WebWithAstha <lodhiastha85@gmail.com>
2024-05-20 15:45:47 +0530

    inCart check added

--- controllers/product_controller.js
+++ controllers/product_controller.js
@@ -1,14 +1,40 @@
 const productModel = require('../models/product.js')
+const cartProductModel = require('../models/cartProduct.js')
 
-exports.getProductsByAllCategories = async(req,res)=>{
-    const categories =['Footwear','Books & stationary','Clothing','Toys']
-    const productByCategory ={}
+// exports.getProductsByAllCategories = async(req,res)=>{
+//     const categories =['Footwear','Books & stationary','Clothing','Toys']
+//     const productByCategory ={}
+//     for (const category of categories) {
+//         const products = await productModel.find({ category });
+//         if(products.length > 0) {
+//             productByCategory[category] =products
+//         }
+//     }
+
+//     return productByCategory;
+// }
+
+
+exports.getProductsByAllCategories = async (req, res) => {
+    const categories = ['Footwear', 'Books & stationary', 'Clothing', 'Toys'];
+    const productByCategory = {};
+
     for (const category of categories) {
         const products = await productModel.find({ category });
-        if(products.length > 0) {
-            productByCategory[category] =products
+
+        if (products.length > 0) {
+            const productsWithCartStatus = await Promise.all(products.map(async (product) => {
+                const isCartProduct = await cartProductModel.findOne({ user: product.user, product: product._id }) ? true : false;
+                return { ...product.toObject(), isCartProduct };
+            }));
+
+            productByCategory[category] = productsWithCartStatus;
         }
     }
+    console.log(productByCategory)
 
-    return productByCategory;
-}
+    return (productByCategory);
+};
+
+
+// const isCartProduct = await cartProductModel.findOne({user:product._id})?true:false
